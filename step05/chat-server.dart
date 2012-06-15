5c598df38e781e5aa9f2b7d1c2923adeb9beb3a9
Seth Ladd <sethladd@dhcp-172-22-70-56.mtv.corp.google.com>
2012-06-14 20:01:13 -0700

    add step 5

---
+++ step05/chat-server.dart
@@ -0,0 +1,89 @@
+#library('chat-server');
+
+#import('dart:io');
+#import('dart:isolate');
+#import('file-logger.dart', prefix: 'log');
+#import('server-utils.dart');
+
+class StaticFileHandler {
+  final String basePath;
+  
+  StaticFileHandler(this.basePath);
+  
+  _send404(HttpResponse response) {
+    response.statusCode = HttpStatus.NOT_FOUND;
+    response.outputStream.close();
+  }
+
+  // TODO: etags, last-modified-since support
+  onRequest(HttpRequest request, HttpResponse response) {
+    final String path = request.path == '/' ? '/index.html' : request.path;
+    final File file = new File('${basePath}${path}');
+    file.exists().then((found) {
+      if (found) {
+        file.fullPath().then((String fullPath) {
+          if (!fullPath.startsWith(basePath)) {
+            _send404(response);
+          } else {
+            file.openInputStream().pipe(response.outputStream);
+          }
+        });
+      } else {
+        _send404(response);
+      }
+    }); 
+  }
+}
+
+class ChatHandler {
+  
+  Set<WebSocketConnection> connections;
+  
+  ChatHandler(String basePath) : connections = new Set<WebSocketConnection>() {
+    log.initLogging('${basePath}/chat-log.txt');
+  }
+  
+  // closures!
+  onOpen(WebSocketConnection conn) {
+    print('new ws conn');
+    connections.add(conn);
+    
+    conn.onClosed = (int status, String reason) {
+      print('conn is closed');
+      connections.remove(conn);
+    };
+    
+    conn.onMessage = (message) {
+      print('new ws msg: $message');
+      connections.forEach((connection) {
+        if (conn != connection) {
+          print('queued msg to be sent');
+          queue(() => connection.send(message));
+        }
+      });
+      time('send to isolate', () => log.log(message));
+    };
+    
+    conn.onError = (e) {
+      print("problem w/ conn");
+      connections.remove(conn); // onClosed isn't being called ??
+    };
+  }
+}
+
+runServer(String basePath, int port) {
+  HttpServer server = new HttpServer();
+  WebSocketHandler wsHandler = new WebSocketHandler();
+  wsHandler.onOpen = new ChatHandler(basePath).onOpen;
+  
+  server.defaultRequestHandler = new StaticFileHandler(basePath).onRequest;
+  server.addRequestHandler((req) => req.path == "/ws", wsHandler.onRequest);
+  server.listen('127.0.0.1', 1337);
+  print('listening for connections on $port');
+}
+
+main() {
+  var script = new File(new Options().script);
+  var directory = script.directorySync();
+  runServer("${directory.path}/client", 1337);
+}
---
+++ step05/client/chat-client.dart
@@ -0,0 +1,115 @@
+#library('chat-client');
+
+#import('dart:html');
+
+// Step 5, import the JSON library
+
+ChatConnection chatConnection;
+MessageInput messageInput;
+UsernameInput usernameInput;
+ChatWindow chatWindow;
+
+class ChatConnection {
+  // Step 06, add webSocket instance field
+  String url;
+  
+  ChatConnection(this.url) {
+    _init();
+  }
+  
+  send(String from, String message) {
+    // Step 5, encode from and message into one JSON string
+  }
+  
+  _receivedEncodedMessage(String encodedMessage) {
+    // Step 5, decode a JSON string and display it in the chat window
+  }
+  
+  _sendEncodedMessage(String encodedMessage) {
+    // Step 6, send the message over the WebSocket
+  }
+  
+  _init() {
+    // Step 6, connect to the WebSocket, listen for events
+  }
+}
+
+abstract class View<T> {
+  final T elem;
+  
+  View(this.elem) {
+    bind();
+  }
+  
+  // bind to event listeners
+  void bind() { }
+}
+
+class MessageInput extends View<InputElement> {
+  MessageInput(InputElement elem) : super(elem);
+  
+  disable() {
+    elem.disabled = true;
+    elem.value = 'Enter username';
+  }
+  
+  enable() {
+    elem.disabled = false;
+    elem.value = '';
+  }
+  
+  String get message() => elem.value;
+  
+  bind() {
+    elem.on.change.add((e) {
+      chatConnection.send(usernameInput.username, message);
+      chatWindow.displayMessage(message, usernameInput.username);
+      elem.value = '';
+    });
+  }
+}
+
+class UsernameInput extends View<InputElement> {
+  UsernameInput(InputElement elem) : super(elem);
+  
+  bind() {
+    elem.on.change.add((e) => _onUsernameChange());
+  }
+  
+  _onUsernameChange() {
+    _enableMessageInput();
+  }
+  
+  _enableMessageInput() {
+    if (!elem.value.isEmpty()) {
+      messageInput.enable();
+    } else {
+      messageInput.disable();
+    }
+  }
+  
+  String get username() => elem.value;
+}
+
+class ChatWindow extends View<TextAreaElement> {
+  ChatWindow(TextAreaElement elem) : super(elem);
+  
+  displayMessage(String msg, String from) {
+    _display("$from: $msg\n");
+  }
+  
+  displayNotice(String notice) {
+    _display("[system]: $notice\n");
+  }
+  
+  _display(String str) {
+    elem.text = "${elem.text}$str";
+  }
+}
+
+main() {
+  chatWindow = new ChatWindow(document.query('#chat-display'));
+  usernameInput = new UsernameInput(document.query('#chat-username'));
+  messageInput = new MessageInput(document.query('#chat-message'));
+  chatConnection = new ChatConnection("ws://127.0.0.1:1337/ws");
+}
